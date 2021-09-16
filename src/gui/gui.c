1cf45e6470cb6dee77aed3f97e09cfbde43a8613
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-09-16 20:09:50 +0700

    make a UI that actually functions

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -2,13 +2,14 @@
 
 void activate(GtkApplication *app, gpointer data)
 {
-    GtkApplicationWindow *app_window;
+    GtkWidget *app_window;
     GtkBuilder *builder;
     
     builder = gtk_builder_new_from_file("editor.ui");
+        
+    app_window = GTK_WIDGET(gtk_builder_get_object(builder, "app_window"));
+    gtk_window_set_application(GTK_WINDOW(app_window), GTK_APPLICATION(app));
     
-    app_window = GTK_APPLICATION_WINDOW(gtk_builder_get_object(builder, "app_window"));
-    
-    gtk_window_present(GTK_WINDOW(app_window));
+    gtk_widget_show(app_window);
 }
 
