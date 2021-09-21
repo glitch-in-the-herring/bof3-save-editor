a427236a6943dc9c1876b26c4af1081cb5691417
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-09-21 19:36:07 +0700

    yeah

--- src/memcard/memcard.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMCARD
-#define MEMCARD
-
-#include <gtk/gtk.h>
-
-int validate_memory_card(unsigned char *memory_card);
-int browse_toc(unsigned char *memory_card);
-
-#endif
--- src/utils/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS
-#define UITLS
-
-#include <stdint.h>
-
-uint32_t convert_little_endian(unsigned char *memory_card, int start, int length);
-
-#endif
