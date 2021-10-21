36d247682b90d09f2d476ea1a5756dff95147d1b
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-21 18:46:54 +0700

    it might be a bad idea to be able to only test this on gh pages

--- characters.js
+++ characters.js
@@ -101,6 +101,6 @@ function show_character(character, stats_e, resist_e, eqp_e, abil_e)
     for (let i = 0; i < keys.length; i++)
     {
         index = keys[i];
-        abil_e[index].value = character.abil_e[0][i];
+        abil_e[index].value = character.abilities[0][i];
     }        
 }
