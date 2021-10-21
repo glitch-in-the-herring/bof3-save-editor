233e713f8e9def1a5e24bf334868a24689f51aab
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-21 09:35:11 +0700

    armor

--- items.js
+++ items.js
@@ -18,9 +18,19 @@ const fishing_array = load_file("items/fishing.txt").split("\n");
 const abil_array = load_file("items/abils.txt").split("\n");
 const vitals_array = load_file("items/vitals.txt").split("\n");
 
-const character_eqp_weapon = document.getElementById("character_eqp_weapon");
-
-for (let i = 0; i < weapon_array.length; i++)
+function load_item_select(combo_boxes, array)
 {
-    combo_box.innerHTML += "<option value=\"" + i.toString()  + "\">" + weapon_array[i] +  "</option>\n";
+    for (let i = 0; i < weapon_array.length; i++)
+    {
+        for (let j = 0; j < combo_boxes.length; j++)
+            combo_boxes[j].innerHTML += "<option value=\"" + i.toString()  + "\">" + array[i] +  "</option>\n";
+    }    
 }
+
+const character_eqp_weapon = document.getElementById("character_eqp_weapon");
+const character_eqp_helm = document.getElementById("character_eqp_helm");
+const character_eqp_shield = document.getElementById("character_eqp_shield");
+const character_eqp_armor = document.getElementById("character_eqp_armor");
+
+load_item_select([character_eqp_weapon], weapon_array);
+load_item_select([character_eqp_helm, character_eqp_shield, character_eqp_armor], armor_array);
