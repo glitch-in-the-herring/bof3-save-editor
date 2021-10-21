3880ca7800ecfc04c6968e2e7ac1e0b8f8b94ab8
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-21 12:45:16 +0700

    test

--- save.js
+++ save.js
@@ -1,6 +1,7 @@
 function save_file(e)
 {
     store_character(e.target.slots[e.target.s[0]].characters[e.target.s[1]], e.target.e[0], e.target.e[1], e.target.e[2]);
+    console.log(e.target.slots);
     for (let i = 0; i < e.target.slots.length; i++)
     {
         save_character_data(e.target.byte_array, e.target.slots[i]);
