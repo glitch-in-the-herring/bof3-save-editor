6fbf20e74fb3bdc5df9c7e97f0f703eb5fb16425
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-21 09:25:58 +0700

    weapons

--- items.js
+++ items.js
@@ -1,16 +1,26 @@
 function load_file(filename)
 {
-    let output;
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", filename, true);
-    xhr.onload = function(e)
+    let result = null;
+    xhr.open("GET", filename, false);
+    xhr.send();
+    if (xhr.readyState === 4 && xhr.status === 200)
     {
-        if (xhr.readyState === 4 && xhr.status === 200)
-        {
-            console.log("yeah");
-            result = output.responseText;
-            return output;
-        }
+        return xhr.responseText;
     }
-    xhr.send(null);
+}
+
+const weapon_array = load_file("items/weapons.txt").split("\n");
+const armor_array = load_file("items/armor.txt").split("\n");
+const acc_array = load_file("items/accs.txt").split("\n");
+const item_array = load_file("items/items.txt").split("\n");
+const fishing_array = load_file("items/fishing.txt").split("\n");
+const abil_array = load_file("items/abils.txt").split("\n");
+const vitals_array = load_file("items/vitals.txt").split("\n");
+
+const character_eqp_weapon = document.getElementById("character_eqp_weapon");
+
+for (let i = 0; i < weapon_array.length; i++)
+{
+    combo_box.innerHTML += "<option value=\"" + i.toString()  + "\">" + weapon_array[i] +  "</option>\n";
 }
