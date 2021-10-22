c5fbb5ce6ecefc1108cb41703fe811cf18392e4d
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-22 07:56:41 +0700

    async when

--- inventory.js
+++ inventory.js
@@ -24,7 +24,8 @@ function get_inv_e()
         tbox.classList.add("narrow");
         tbox.setAttribute("disabled", "");
         load_item_select(select, item_array);
-        li.appendChild(select);        
+        li.appendChild(select);
+        li.appendChild(tbox);
         output["inv"][i][0] = select;
         output["inv"][i][1] = tbox;
         inv_list.appendChild(li);
