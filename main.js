9c6ae95176aae219a182bf0b511646b11444a338
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-22 21:00:34 +0700

    that's a lot of red

--- main.js
+++ main.js
@@ -28,6 +28,9 @@ load_item_select([char_e.eqp[0]], weapon_array);
 load_item_select([char_e.eqp[1], char_e.eqp[2], char_e.eqp[3]], armor_array);
 load_item_select([char_e.eqp[4], char_e.eqp[5]], acc_array);
 
+document.getElementById("loading").style.display = "none";
+document.getElementById("main").style.display = "block";
+
 function on_file_open()
 {
     const reader = new FileReader();
