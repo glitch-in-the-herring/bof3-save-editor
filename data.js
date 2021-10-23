946734eff25246efac005fb1700f1ca219e48206
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-23 08:24:21 +0700

    save it

--- data.js
+++ data.js
@@ -112,6 +112,7 @@ function store_char(char_e, char)
 
 function store_inv(inv_e, inv)
 {
+    console.log("test");
     for (let i = 0; i < inv_e.length; i++)
     {
         inv[i][0] = inv_e.inv[i][0].value;
