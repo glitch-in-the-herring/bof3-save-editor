28d2c2cb0e5738ac41794b3a7d4d71a4ae602fc5
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-10-23 07:43:54 +0700

    familiar

--- inventory.js
+++ inventory.js
@@ -10,17 +10,23 @@ function get_inv_e()
     {
         output["inv"][i] = [];
         li = document.createElement("li");
-        select = document.createElement("select");
+        select = document.createElement("input");
         tbox = document.createElement("input");
-        select.classList.add("disabled");
+        select.setAttribute("type", "number");
+
+        select.setAttribute("min", "0");
+        select.setAttribute("max", "92");
         select.setAttribute("disabled", "");
+        select.classList.add("disabled");
+        select.classList.add("narrow");
+
         tbox.setAttribute("type", "number");
         tbox.setAttribute("min", "0");
         tbox.setAttribute("max", "255");
+        tbox.setAttribute("disabled", "");
         tbox.classList.add("disabled");
         tbox.classList.add("narrow");
-        tbox.setAttribute("disabled", "");
-        load_item_select([select], item_array[0]);
+
         li.appendChild(select);
         li.appendChild(tbox);
         inv_list.appendChild(li);        
@@ -31,27 +37,32 @@ function get_inv_e()
     output["inv_prev_button"] = document.getElementById("inv_prev_button");
     output["inv_next_button"] = document.getElementById("inv_next_button");
     output["inv_label"] = document.getElementById("inv_type_indicator");
+    output["inv_info"] = document.getElementById("inv_info");
 
     return output;
 }
 
 function show_inv(inv_e, inv)
 {
-    console.log("4");
     let load_array;
     if (inv_e.cur.inv != 3)
         load_array = item_array[inv_e.cur.inv];
     else
         load_array = item_array[3].concat(item_array[4]);
 
-    document.getElementById("loading").style.display = "block";
-    document.getElementById("main").style.display = "none";
     for (let i = 0; i < 128; i++)
     {
-        load_item_select(inv_e.inv[i], load_array);
         inv_e.inv[i][0].value = inv[i][0];
+        inv_e.inv[i][0].setAttribute("max", load_array.length);
         inv_e.inv[i][1].value = inv[i][1];
     }
-    document.getElementById("loading").style.display = "none";
-    document.getElementById("main").style.display = "block";
+
+    inv_e.inv_info.textContent = "";
+    let li;
+    for (let i = 0; i < load_array.length; i++)
+    {
+        li = document.createElement("li");
+        li.innerHTML = load_array[i];
+        inv_e.appendChild(li);
+    }
 }
