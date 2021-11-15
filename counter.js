91843af03c5386b38ff2f500363ece09d3d231b5
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-11-15 17:05:47 +0700

    add new

--- counter.js
+++ counter.js
@@ -1,4 +1,14 @@
-function get_counter_e
+function get_counter_e()
 {
-    
+    let counter_e = {};
+    //counter_e["timer"] = document.getElementById("timer_timer");
+    counter_e["celerity"] = document.getElementById("timer_celerity");
+    counter_e["bonebreak"] = document.getElementById("timer_bonebreak");
+
+    return counter_e;
+}
+
+function get_counter_status(counter_e)
+{
+    return [counter_e["celerity"].checked, counter_e["bonebreak"].checked];
 }
